5f9f2c1a8e8880b190992ac3887aefb3b0751c63
arupsarkar <sarkar.arup@gmail.com>
2022-06-26 14:44:05 -0400

    Added PWA capabilities, use this for PWA development

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import App from "./app";
+// import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './service-worker'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
 root.render(<App />)
+console.log('Registering service worker', 'Starting...')
+
+// serviceWorker.register()
+registerServiceWorker()
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,60 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path');
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html", 
-  filename: "./index.html"
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+// const htmlPlugin = new HtmlWebPackPlugin({
+//   template: "./src/index.html",
+//   filename: "./index.html",
+// });
+
+const htmlWebPackPlugin =  new HtmlWebPackPlugin({
+  title: "PWA Webpack Demo",
+  template: "./src/index.html",
+  filename: "./index.html",  
 });
+
+const workBoxPlugin_Generate = new WorkboxPlugin.GenerateSW({
+  // these options encourage the ServiceWorkers to get in there fast
+  // and not allow any straggling "old" SWs to hang around
+  clientsClaim: true,
+  skipWaiting: true,
+});
+
+const processEnvPlugin = new webpack.DefinePlugin({
+  "process.env.PUBLIC_URL": JSON.stringify(process.env.PUBLIC_URL),
+});
+
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: ["./src/index.tsx",],
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },  
-  output: { // NEW
-    path: path.join(__dirname, 'dist'),
-    filename: "[name].js"
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    // NEW
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
   }, // NEW Ends
-  plugins: [htmlPlugin],
+  plugins: [htmlWebPackPlugin, workBoxPlugin_Generate, processEnvPlugin],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: ["ts-loader"],
-      },      
+      },
       // css rules
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
-    ]
-  }
-}
+    ],
+  },
+};
